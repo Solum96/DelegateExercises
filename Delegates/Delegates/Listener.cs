c2a673a45e28c020806130f04c02e3dd48c8d96b
Nils Lundell <nils.solumfaeste@gmail.com>
2019-10-17 13:35:02 +0200

    Added ex2

---
+++ Delegates/Delegates/Listener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Listener
+    {
+        string CurrentOrder { get; set; }
+        public Listener()
+        {
+        }
+
+        internal void OnGiveOrder()
+        {
+            Console.WriteLine("Yaas queen!");
+        }
+    }
+}
